00ab81a8b37a94d2d89130d9825003f714e3009e
Patrick O'Neill <synapse.and.syntax@gmail.com>
2013-10-30 04:56:47 -0400

    init commit for setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='we',
+      version='0.0.1',
+      author="Patrick O'Neill",
+      author_email="pon2@umbc.edu",
+      #py_modules=['we'],
+      packages=['we'],
+      package_dir={'we':"src"}
+      )
